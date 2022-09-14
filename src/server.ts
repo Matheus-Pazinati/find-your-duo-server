a21da30e43e11230a246840c500c19349151726d
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-14 05:00:24 -0300

    Cria as rotas da aplicacao

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,25 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+  return res.json([])
+})
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([])
+})
+
+app.get('/games/:id/ads', (req, res) => {
   return res.json([
     { id: 1, name: 'Anuncio 1' },
     { id: 2, name: 'Anuncio 2' },
     { id: 3, name: 'Anuncio 3' },
     { id: 4, name: 'Anuncio 4' },
   ])
+})
+
+app.get('/ads/:id/discord', (req, res) => {
+  return res.json([])
 })
 
 app.listen(3333)
